8c1693279693c6481e33906eecef1196b63e154e
Guga Figueiredo <gugabfigueiredo@gmail.com>
2022-10-17 02:31:38 -0300

    improving typings

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export interface Emitter {
 export class Logger {
 
     emitter: Emitter;
-    context: object;
+    context: {[k: string]: any};
     level: string;
 
     constructor(emitter: Emitter, level: string = LOG_LEVEL.ALL, context: object = {}) {
@@ -14,36 +14,36 @@ export class Logger {
         this.level = level
     }
 
-    C(tags: object): Logger {
+    C(tags: {[k: string]: any}): Logger {
         return new Logger(this.emitter, this.level, { ...this.context, ...tags })
     }
 
-    with(tags: object) : void {
+    with(tags: {[k: string]: any}) : void {
         this.context = { ...this.context, ...tags }
     }
 
-    print(tags: object) {
-        this.emitter.log(JSON.stringify({timestamp: new Date().toISOString(), ...tags}))
+    log(tags: {[k: string]: any}) {
+        this.emitter.log(JSON.stringify({timestamp: new Date().toISOString(), ...this.context, ...tags}))
     }
 
-    I(message: string, tags?: object) {
+    I(message: string, tags?: {[k: string]: any}) {
         if ([LOG_LEVEL.INFO, LOG_LEVEL.DEBUG, LOG_LEVEL.ALL].includes(this.level))
-            this.print({level: "INFO", message: message, ...tags});
+            this.log({level: "INFO", message: message, ...tags});
     }
 
-    W(message: string, err: any, tags?: object) {
+    W(message: string, err: any, tags?: {[k: string]: any}) {
         if ([LOG_LEVEL.WARN, LOG_LEVEL.DEBUG, LOG_LEVEL.ALL].includes(this.level))
-            this.print({level: "WARN", message: message, err: err, ...tags});
+            this.log({level: "WARN", message: message, err: err, ...tags});
     }
 
-    E(message: string, err: any, tags?: object) {
+    E(message: string, err: any, tags?: {[k: string]: any}) {
         if ([LOG_LEVEL.ERROR, LOG_LEVEL.DEBUG, LOG_LEVEL.ALL].includes(this.level))
-            this.print({level: "ERROR", message: message, err: err, ...tags});
+            this.log({level: "ERROR", message: message, err: err, ...tags});
     }
 
-    F(message: string, err: any, tags?: object) {
+    F(message: string, err: any, tags?: {[k: string]: any}) {
         if ([LOG_LEVEL.ERROR, LOG_LEVEL.DEBUG, LOG_LEVEL.ALL].includes(this.level))
-            this.print({level: "FATAL", message: message, err: err, ...tags});
+            this.log({level: "FATAL", message: message, err: err, ...tags});
         process.exit(1);
     }
 }
